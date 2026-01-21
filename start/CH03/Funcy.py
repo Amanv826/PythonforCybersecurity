65b61b0ae5b93bf879e5ae33d02502c6d7390720
amanv826 <vohra.aman@student.greenriver.edu>
2026-01-21 11:45:17 -0800

    Loop

--- start/CH03/Funcy.py
+++ start/CH03/Funcy.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
 # example workign with Functions
-#By 
+#By Aman Vohra 1/21/26
+
+#prompt use to answer a question
+good_day_question = "Is today a good day? (y/n) "
+user_response = input(good_day_question)
+#Store in variable
+
+#if yes print a response
+if user_response == "y":
+    for count in range(10):
+        print("Yes it is")
+elif user_response == "Y":
+    print("That's amazing")
+else:
+    print("I hope it gets better.")
+
+
