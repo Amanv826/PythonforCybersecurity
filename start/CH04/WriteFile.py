85d56d5993fd5a6593d9179f7499d3e17c8f11b5
amanv826 <vohra.aman@student.greenriver.edu>
2026-01-26 12:08:00 -0800

    Change

--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -8,18 +8,17 @@ hackerinfo = open("hackme.txt", "w")
 #questions to collect info
 print("Answer the following questions")
 
-name = input("What is your name?")
-favorite_color = input ("What is your favorite color?")
-first_pet_name = input ("what is your first pet's name?")
-mothers_maiden_name = input ("What is your mohter's maiden name?")
-elementary_school = inut ("What elementary school did you go to?")
+name = input("What is your name? ")
+favorite_color = input ("What is your favorite color? ")
+first_pet_name = input ("what is your first pet's name? ")
+mothers_maiden_name = input ("What is your mohter's maiden name? ")
+elementary_school = input ("What elementary school did you go to? ")
 
 #write to the file
-hackerinfo.writer("Name:" + name + "\n")
-hackerinfo.writer("Favorite color: " + favorite_color + "\n")
-hackerinfo.writer("First pet's name: " + first_pet_name + "\n")
-hackerinfo.writer("Mother's maiden name: " + mothers_maiden_name + "\n")
-hackerinfo.writer("Elementary school: " + elementary_school + "\n")
+hackerinfo.write("Name:" + name + "\n")
+hackerinfo.write("Favorite color: " + favorite_color + "\n")
+hackerinfo.write("First pet's name: " + first_pet_name + "\n")
+hackerinfo.write("Mother's maiden name: " + mothers_maiden_name + "\n")
+hackerinfo.write("Elementary school: " + elementary_school + "\n")
 
-#close the file
-hackerinfo.closed()
+
