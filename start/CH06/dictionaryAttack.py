1ec9797dd9e59ea8ee4b45de023b95c5746fee7d
amanv826 <vohra.aman@student.greenriver.edu>
2026-02-23 11:14:09 -0800

    Done

--- start/CH06/dictionaryAttack.py
+++ start/CH06/dictionaryAttack.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
 # Script that performs a dictionary attack against known password hashes
 # Needs a dictionary file, suggested to use https://github.com/danielmiessler/SecLists/tree/master/Passwords/Common-Credentials
-# By 
+# By Aman Vohra on 2/9/2026
+
+#import moudules
+import os
+import crypt
+#function to test password
+def test_password(algo_salt, hashed_password, password_guess):
+    #Use salt to hash guess
+    hashed_guess = crypt.crypt(password_guess, algo_salt)
+    #Compare salted guess against hashed password
+    if hashed_guess == hashed_password: 
+        return True
+    return False
+#load dictionary file
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+f = open(dir_path + "/top10000000.txt", "r")
+passwords = f .readlines()
+#Prompt user for Algorithm/Salt
+algorithm_salt = input("What is the algorith and salt?")
+#Prompt user for salted hash
+hashed_pasword = input ("What is the full hased password?")
+#Loop through each password
+for password in passwords:
+    password = password.strip()
+    result = test_password(algorithm_salt, hashed_pasword, password)
+    if result:
+        print("Match found: {0}".format (password))
+        break
--- start/CH07/Find404.py
+++ start/CH07/Find404.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
 # Script that scans web server logs for 404 errors
-# By 
+# By Aman Vohra 2/18/26
+
+import os
+
+#prompt for file and open
+log_file = input("Which file do not want to scan?")
+dir_path = os.path.dirname(os.path.realpath(__file__))
+f= open(dir_path + "/" + log_file, "r")
+
+#Read file line by line
+while True: 
+    line = f.readline()
+    if not line:
+        break
+    #look for 404 and print
+    if "404" in line: 
+        print(line.strip())
+#close file
+f.close()
