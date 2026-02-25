9971562c6cc7a13fa13947322d192e2efd5c04e1
amanv826 <vohra.aman@student.greenriver.edu>
2026-02-25 10:45:47 -0800

    Dad

--- start/CH08/PeopleInSpace1.py
+++ start/CH08/PeopleInSpace1.py
@@ -1,1 +1,28 @@
 # paste code here
+
+import requests
+
+url = "https://icanhazdadjoke.com/?"
+
+payload={}
+headers = {
+  'Accept': 'application/json'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
+
+import requests
+
+url = "https://randomuser.me/api/"
+
+payload={}
+headers = {
+  'Accept': 'application/json'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
+
